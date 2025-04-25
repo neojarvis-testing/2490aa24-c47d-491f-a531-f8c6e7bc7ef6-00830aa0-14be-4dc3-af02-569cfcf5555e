e69b3e284a25abf0d451170feb9962d377b46bdf
LSrihith <srihithlagisetty@gmail.com>
2025-04-25 05:00:53 +0000

    srihith intial commit

--- Project/src/test/java/utils/Reporter.java
+++ Project/src/test/java/utils/Reporter.java
@@ -1,5 +1,47 @@
 package utils;
-
-public class Reporter {
-    
+ 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import com.aventstack.extentreports.*;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+ 
+public class Reporter extends Base{
+    private static ExtentReports extent;
+ 
+    public static ExtentReports generateReport(String reportName) {
+        if (extent == null) {
+            String timestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+            String reportsDir = System.getProperty("user.dir") + "/reports";
+            String reportPath = reportsDir + "/" + (reportName.isEmpty() ? "Test_Report" : reportName) + "_" + timestamp
+                    + ".html";
+            new File(reportsDir).mkdirs();
+ 
+            ExtentSparkReporter spark = new ExtentSparkReporter(reportPath);
+            spark.config().setReportName(reportName);
+            spark.config().setDocumentTitle("Automation Report");
+ 
+            extent = new ExtentReports();
+            extent.attachReporter(spark);
+            extent.setSystemInfo("OS", System.getProperty("os.name"));
+            extent.setSystemInfo("User", System.getProperty("user.name"));
+        }
+        return extent;
+    }
+ 
+    public static void attachScreenshot(String filename, ExtentTest test, String description) {
+        try {
+            String timestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+            String screenshotPath = System.getProperty("user.dir") + "/reports/" + filename + "_" + timestamp + ".png";
+            File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            org.apache.commons.io.FileUtils.copyFile(src, new File(screenshotPath));
+ 
+            test.info(description, MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+        } catch (Exception e) {
+            e.getMessage();
+        }
+    }
+ 
 }
